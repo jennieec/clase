f7d2a7e735d12626de869cd99a6d9c366a173ade
Jonathangp18 <62915143+Jonathangp18@users.noreply.github.com>
2020-05-01 19:02:46 -0500

    Update student.py

--- student.py
+++ student.py
@@ -1,14 +1,14 @@
 class student:
 
-def _init_(self, code, name, age, gender, carrreer):
+def _init_(self, code, name, aje, gender, carrreer):
 self.code = code
 self.name = name
-self.age = age
+self.age = aje
 self.gender = gender
 self.carrreer = carrreer
 
-def set_age(self, age):
-    self.age = age
+def set_age(self, aje):
+    self.age = aje
 
 def set_gender(self, gender):
     self.gender = gender
@@ -16,8 +16,8 @@ def set_gender(self, gender):
 def set_career(self, carrreer):
     self.carrreer = carrreer
 
-def get_age(self):
-    return ("edad: " + self.age)
+def get_aje(self):
+    return ("edad: " + self.aje)
 
 def get_gender(self):
     return ("genero: " + self.gender)
