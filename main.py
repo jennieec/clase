8025a94afad9a266c49915d9f81da8d7854e0f94
Jonathangp18 <62915143+Jonathangp18@users.noreply.github.com>
2020-05-01 19:06:01 -0500

    Update main.py

--- main.py
+++ main.py
@@ -7,18 +7,18 @@ def crea_estudiantes(count_estudiantes):
 	while count < int(count_estudiantes):
     student_code = input ("dame la matricula: ")
     student_name = input ("dame el nombre: ")
-    student_age = input ("dame la edad: ")
+    student_aje = input ("dame la edad: ")
     student_gender = input ("dame el genero: ")
     student_carreer = input ("dame la carrera: ")
 
-    list_students.append(student(student_code, student_name, student_age, student_gender, student_carreer))
+    list_students.append(student(student_code, student_name, student_aje, student_gender, student_carreer))
 
     count= count+1
 return list_students
 
 def ordena_edades(list_students):
 	
-	student_age.sort() 
+	student_aje.sort() 
 	print list_students[]:
 
 def separa_generos(list_students):
@@ -36,13 +36,13 @@ def main():
     list_stud=[]
 
     while options != "0":
-        options=input("menu opciones 1. crea estudiantes 2.ordena edades  3.separa generos 0. salir: ")
+        options=input("menu opciones 1. crea estudiantes 2.ordena edodes  3.separa generos 0. salir: ")
 
         if options == "1":
             print("crea estudiantes")
         
         if options == "2":
-            print("ordena edades")
+            print("ordena edodes")
         
         if options == "3":
             print("separa generos")
